5d17ea439e0e5bb586e520b7b72c0d830ef417d8
Ariyan_Ali <ariyanali42@gmail.com>
2025-07-25 03:42:22 -0700

    some changes in Post Class

--- CW_6_Part_2.py
+++ CW_6_Part_2.py
@@ -21,22 +21,18 @@ class StandardUser(UserProfile):
         self.post.append(Post(content))
 
         if len(self.post) >= self.post_limit:
-            raise AttributeError(
-                f"You've reached your limit. your limit is {self.post_limit}"
-            )
+            raise AttributeError(f"You've reached your limit. your limit is {self.post_limit}")
         return
 
 
 class PermiumUser(UserProfile):
-    def __init__(self, username, email, sign_up_date, analytics, post_edit, post_limit):
+    def __init__(self, username, email, sign_up_date, post_limit):
         super().__init__(username, email, sign_up_date)
 
-        self.analytics = analytics
-        self.post_edit = post_edit
         self.post_limit = post_limit
         self.post = []
 
-    def create_post(self, content):
+    def create_post_permium(self, content):
 
         self.post.append(Post(content))
 
@@ -45,18 +41,26 @@ class Post:
     def __init__(self, content, number_of_likes=0, number_of_shares=0):
 
         self.content = content
+        # self.analytics = analytics
+        # self.post_edit = post_edit
         self.number_of_likes = number_of_likes
         self.number_of_shares = number_of_shares
 
 
 stu = StandardUser("ariyan", "asb@email.com", "July 2025", 1)
 
-pru = PermiumUser("ali", "ali@email.com", "july 2024", )
+pru = PermiumUser("ali", "ali@email.com", "july 2024", "No Limit")
+
+
+
+
+
+pru.create_post_permium("hello wordl")
+
+print(len(pru.post))
+
 
 stu.create_post("Hello World")
 stu.create_post("Hello World2")
-# stu.create_post("Hello World3")
-
+stu.create_post("Hello World3")
 print(len(stu.post))
-
-
