8883bdb1a05fe3263cff6a6ceae9e4a3d7750357
Ariyanali <Ariyanali42@gmail.com>
2025-10-16 06:33:46 -0700

    DONE

--- test_math_utils.py
+++ test_math_utils.py
@@ -6,8 +6,22 @@ from .math_utils import MathUtils
 
 math_utils = MathUtils()
 
-def test_add_pos():
-    assert math_utils.add(3, 8) == 11
+
+@pytest.fixture
+def numbers():
+    return {"test_add_pos":(3, 8), 
+            "test_add_neg":(-20, -50), 
+            "test_divide_n":(0, 3), 
+            "test_divide_z":(3, 0), 
+            "test_is_even_1": (8), 
+            "test_is_even_2": (3), 
+            "test_is_even_3": (0)
+            }
+
+
+def test_add_pos(numbers):
+    a , b = numbers["test_add_pos"]
+    assert math_utils.add(*numbers["test_add_pos"]) == 11
 
 def test_add_neg():
     assert math_utils.add(-20, -50) == -70
@@ -20,11 +34,11 @@ def test_divide_z():
         math_utils.divide(3, 0)
 
 
-def test_is_even():
+def test_is_even_1():
     assert math_utils.is_even(8)
 
-def test_is_even():
-    assert math_utils.is_even(7)
+def test_is_even_2():
+    assert math_utils.is_even(3)
 
-def test_is_even():
+def test_is_even_3():
     assert math_utils.is_even(0)
