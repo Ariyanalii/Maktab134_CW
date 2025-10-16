31cf59277d8026ee25935056c1697181fb75071a
Ariyanali <Ariyanali42@gmail.com>
2025-10-16 05:46:51 -0700

    ADD(math_utils_add)

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,2 +1,25 @@
+import pytest
 
+
+from .math_utils import MathUtils
+
+
+math_utils = MathUtils()
+
+def test_add_pos():
+    assert math_utils.add(3, 8) == 11
+
+def test_add_neg():
+    assert math_utils.add(-20, -50) == -70
+
+def test_divide_n():
+    assert math_utils.divide(0, 3) == 3
+
+def test_divide_z():
+    with pytest.raises(ZeroDivisionError):
+        math_utils.divide(3, 0)
+
+
+def test_is_even():
+    assert math_utils.is_even(8)
 
